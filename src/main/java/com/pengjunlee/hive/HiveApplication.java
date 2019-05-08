206647dc76cac119469cd253cc9281d5051b4b20
genie <genie@anve.com>
2019-05-08 09:46:52 +0800

    0508-0946

--- src/main/java/com/pengjunlee/hive/HiveApplication.java
+++ src/main/java/com/pengjunlee/hive/HiveApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HiveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HiveApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HiveApplication.class, args);
+    }
 }
---
+++ src/main/java/com/pengjunlee/hive/util/Test.java
@@ -0,0 +1,12 @@
+package com.pengjunlee.hive.util;
+
+/**
+ * Created by genie on 2018/11/19.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Object result_str = 500;
+    }
+
+}
